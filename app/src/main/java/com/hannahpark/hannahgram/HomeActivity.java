6912eec36a2eefcc970fedaa0028758fabd4ce77
Hannah Park <hannahpark@fb.com>
2018-07-10 10:55:18 -0700

    implemented refresh and createPost

--- app/src/main/java/com/hannahpark/hannahgram/HomeActivity.java
+++ app/src/main/java/com/hannahpark/hannahgram/HomeActivity.java
@@ -1,22 +1,39 @@
 package com.hannahpark.hannahgram;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.hannahpark.hannahgram.model.Post;
 import com.parse.FindCallback;
 import com.parse.ParseException;
-import com.parse.ParseQuery;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.File;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigationView;
+    public static final String imagePath = "/Users/hannahpark/Downloads/IMG_3731.JPG";
+    private EditText descriptionInput;
+    private Button refreshButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        refreshButton = (Button) findViewById(R.id.refresh_button);
+        descriptionInput = (EditText) findViewById(R.id.etDescription);
+
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 //
@@ -29,14 +46,42 @@ public class HomeActivity extends AppCompatActivity {
 //            }
 //        });
 
-        //grab posts
-        ParseQuery.getQuery(Post.class).findInBackground(new FindCallback<Post>() {
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void done(List<Post> objects, ParseException e) {
+            public void onClick(View view) {
+                loadTopPosts();
+            }
+        });
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.post_button:
+                        final String description = descriptionInput.getText().toString();
+                        final ParseUser user = ParseUser.getCurrentUser();
+                        final File file = new File(imagePath);
+                        final ParseFile parseFile = new ParseFile(file);
+                        createPost(description, parseFile, user);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+
+    private void createPost(String description, ParseFile imageFile, ParseUser user) {
+        final Post newPost = new Post();
+        newPost.setDescription(description);
+        newPost.setImage(imageFile);
+        newPost.setUser(user);
+
+        newPost.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
                 if(e == null) {
-                    for(int i = 0; i < objects.size(); ++i) {
-                        Log.d("HomeActivity", "Post[" + i + "] = " + objects.get(i).getDescription());
-                    }
+                    Log.d("HomeActivity", "Create post success!");
                 } else {
                     e.printStackTrace();
                 }
@@ -44,5 +89,29 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
+    public void setBottomNavigationView(BottomNavigationView bottomNavigationView) {
+        this.bottomNavigationView = bottomNavigationView;
+    }
 
+    private void loadTopPosts() {
+        final Post.Query postQuery = new Post.Query();
+        postQuery.getTop().withUser();
+
+        //grab posts
+        postQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if(e == null) {
+                    for(int i = 0; i < objects.size(); ++i) {
+                        Log.d("HomeActivity", "Post[" + i + "] = "
+                                + objects.get(i).getDescription()
+                                + "\nusername = " + objects.get(i).getUser().getUsername()
+                        );  //end of log.d
+                    }
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
